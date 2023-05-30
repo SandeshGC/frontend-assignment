49407d70fc88cd68b3bd60e125b18cb360d8a5e1
SandeshGC <gcsandesh01@gmail.com>
2023-05-30 15:00:58 +0545

    style home page

--- app/components/HomeAlbumCard.tsx
+++ app/components/HomeAlbumCard.tsx
@@ -8,48 +8,44 @@ type Props = {
 const HomeAlbumCard = ({ album }: Props) => {
 	// console.log(album?.images[0].url, "img")
 	return (
-		<div className="shadow my-4 p-4 rounded-lg border hover:shadow-lg group transition duration-500 ease-in-out">
-			<div className="relative h-[350px] max-w-[350px] group mb-4 rounded-md object-contain">
+		<div className="shadow my-4 p-4 max-w-[400px] rounded-lg border hover:shadow-lg group transition duration-500 ease-in-out">
+			<div className="relative h-[350px] w-full mx-auto group mb-4 rounded-md overflow-hidden">
 				<Image
 					src={album?.images[0].url}
 					fill={true}
 					alt={album.name}
-					className="rounded-md object-cover group-hover:scale-105 group-hover:opacity-80 transition duration-500 ease-in-out"
+					className="rounded-md object-contain h-[300px] w-full group-hover:scale-110 group-hover:opacity-80 transition duration-500 ease-in-out"
 					unoptimized
 				/>
 			</div>
-			{/* <img
-				src={album?.images[0].url}
-				width={250}
-				height={250}
-				alt={album.name}
-				className="border-2 rounded-md"
-			/> */}
-			<div className="">
-				<h1 className="font-bold inline text-2xl leading-relaxed hover:text-highlight duration-300 transition ease-in-out">
+
+			<div className="group">
+				<h1 className="font-bold inline text-2xl leading-relaxed group-hover:text-highlight duration-300 transition ease-in-out">
 					{album.name}
 				</h1>
 				<h6 className="text-xs text-gray-500">{album.label}</h6>
 				<h2>
 					Artists:{" "}
 					{album?.artists?.map((artist: any) => (
-						<span key={artist.id}>{artist.name}</span>
+						<span key={artist.id} className="mr-2">
+							{artist.name}
+						</span>
 					))}
 				</h2>
-				{album?.genres?.length ? (
-					<p>
-						Genre:{" "}
+				<p>
+					Genre:{" "}
+					{album?.genres?.length ? (
 						<span
 							className="bg-gray-400 text-white px-3 rounded-full py-1 text-sm
 					"
 						>
 							Pop
 						</span>
-					</p>
-				) : (
-					<p>No Genre Specified</p>
-				)}
-				<p>Release date: {album.release_date}</p>
+					) : (
+						<span className="text-gray-400">-</span>
+					)}
+				</p>
+				<p>Year: {new Date(album.release_date).getFullYear()}</p>
 			</div>
 		</div>
 	)
