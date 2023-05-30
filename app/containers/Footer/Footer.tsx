ab272d1df553b2ad8e86ef60a92106a3f13f163c
SandeshGC <gcsandesh01@gmail.com>
2023-05-30 22:16:52 +0545

    create footer

--- app/containers/Footer/Footer.tsx
+++ app/containers/Footer/Footer.tsx
@@ -3,7 +3,15 @@ import React from "react"
 type Props = {}
 
 const Footer = (props: Props) => {
-	return <footer className="mt-auto">Footer</footer>
+	return (
+		<footer className="mt-auto bg-gray-700 text-gray-50">
+			<div className="container mx-auto p-4">
+				<div className="row">
+					&copy; 2023 - <a href="https://www.gcsandesh.com.np">Sandesh GC</a>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
